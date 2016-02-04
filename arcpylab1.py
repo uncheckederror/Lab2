29f86782d6b690ea95ea6c920af0f46216c839d8
Thomas Ryan <uncheckederror@users.noreply.github.com>
2016-02-03 16:42:37 -0800

    Updated to include working Popcount
    Fixed.

--- arcpylab1.py
+++ arcpylab1.py
@@ -5,21 +5,20 @@
 #Import modules
 import arcpy
 import sys
+import os
 sys.path.append('C:\\Program Files (x86)\\ArcGIS\\Desktop10.3\\bin')
 sys.path.append('C:\\Program Files (x86)\\ArcGIS\\Desktop10.3\\arcpy')
 sys.path.append('C:\\Program Files (x86)\\ArcGIS\\Desktop10.3\\ArcToolbox\\Scripts')
 
 #Set workspace and grab input files
 from arcpy import env
-env.workspace = r"C:\Users\tomaryan\Downloads\\"
+env.workspace = r"C:\Users\Thomas\Desktop\Lab 1 Geog 458\\"
 env.overwriteOutput = True
-input_shapefile = r"C:\Users\tomaryan\Downloads\saep_bg10.shp"
-input_table = r"C:\Users\tomaryan\Downloads\WashingtonFIPS.dbf"
+input_shapefile = r"C:\Users\Thomas\Desktop\Lab 1 Geog 458\saep_bg10.shp"
+input_table = r"C:\Users\Thomas\Desktop\Lab 1 Geog 458\WashingtonFIPS.dbf"
 
 #Shapefile to GeoJSON conversion
 from subprocess import call
-import os
-os.chdir(r"C:\Users\tomaryan\Downloads")
 os.environ["GDAL_DATA"] = "C:\\OSGeo4W\\share\\gdal"
 os.environ["GDAL_DRIVER_PATH"] = "C:\\OSGeo4W\\bin\\gdalplugins"
 os.environ["PROJ_LIB"] = "C:\\OSGeo4W\\share\\proj"
@@ -27,12 +26,12 @@ os.environ["PATH"] = "C:\\OSGeo4W\\bin;"+os.environ["PATH"]+";C:\\OSGeo4W\\apps\
 
 #Create cursor and list
 table_cursor = arcpy.da.UpdateCursor(input_table,["CountyName", "FIPSCounty"])
-countypoplist = []
+countypopdict = dict()
 
 #Create GeoJSON files for each county containing only FIPS matched blocks
 for cur_FIPS in table_cursor:
     
-    cur_shapefile = arcpy.CopyFeatures_management(str(input_shapefile), str(cur_FIPS[0]))
+    cur_shapefile = arcpy.CopyFeatures_management(str(input_shapefile), str(cur_FIPS[0]) + ".shp")
     arcpy.AddField_management(cur_shapefile, "CountyName", "TEXT", "")
     shapefile_cursor = arcpy.da.UpdateCursor(cur_shapefile,["COUNTYFP10","POP2013","FID", "CountyName"])
     countypop = 0
@@ -44,23 +43,24 @@ for cur_FIPS in table_cursor:
         elif cur_row[0] != cur_FIPS[1]:
             shapefile_cursor.deleteRow()
             
+    countypopdict[int(countypop)] = str(cur_FIPS[0])
+    GeoJSONoutputfile = r"C:\Users\Thomas\Desktop\Lab 1 Geog 458\\" + str(cur_FIPS[0]) + ".geojson"
+    print cur_shapefile
+    print arcpy.Exists(cur_shapefile)
+    call(['C:\\OSGeo4W\\bin\\ogr2ogr','-f','GeoJSON','-t_srs','WGS84','-s_srs','EPSG:26913',
+          GeoJSONoutputfile,
+          cur_shapefile])
     del shapefile_cursor
-    countypoplist.append(str(countypop) + ": " + str(cur_FIPS[0]))
-    GeoJSONoutputfile = r"C:\Users\tomaryan\Downloads\\" + str(cur_FIPS[0]) + ".geojson"
-    shapefiletoconvert = r"C:\Users\tomaryan\Downloads\\" + str(cur_FIPS[0]) + ".shp"
-    call(['C:\\OSGeo4W\\bin\\ogr2ogr',
-      '-f','GeoJSON','-t_srs','WGS84',
-      '-s_srs','EPSG:26913',
-      GeoJSONoutputfile,
-      shapefiletoconvert])
     
 del table_cursor
-    
+
 #Sort and print county population values
-countypoplist.sort()
-countypoplist.reverse()
+tosort = dict.keys()
+tosort = tosort.sort()
+tosort = tosort.reverse()
+
 print "A list of top 10 most populated county's in Washington:"
 i = 0
 while i < 10:
-    print countypoplist[i]
+    print countypopdict[tosort[i]] + ": " + str(tosort[i])
     i += 1
